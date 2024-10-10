2ce7c92ceccfee141b6d1aa1dc4a4f75fa55e80c
Nziza-Prince <nzizaprince7@gmail.com>
2024-10-10 22:33:13 +0200

    made dynamic theming

--- myTodoApp/src/components/Notification.jsx
+++ myTodoApp/src/components/Notification.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ThemeContext } from "../store/ThemeContext";
 const Notification = () => {
+  const {isDark} = useContext(ThemeContext)
   return (
     <div>
       <ToastContainer 
       position="top-right"
-      theme="dark"
+      theme={isDark?'dark':'light'}
       />
     </div>
   );
