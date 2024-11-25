7d51aedfabd9ecd4cbe4b825db87911c94d02151
Nziza-Prince <nzizaprince7@gmail.com>
2024-11-25 18:26:48 +0200

    switched to postgres

--- backend/database/databaseConn.js
+++ backend/database/databaseConn.js
@@ -1,17 +1,22 @@
-const {Pool } = require('pg')
+const { Pool } = require('pg');
+const fs = require('fs')
 
+const caCertificate = fs.readFileSync('/home/nziza/Documents/code/fullstacktodoapp/backend/aiven-ca.pem').toString()
 const pool = new Pool({
-    user:"postgres",
-    host:"localhost",
-    database:"todo",
-    password:"co2hno3",
-    port:"5432"
-})
+   user:"postgres",
+   host:"localhost",
+   database:"todo",
+   password:"co2hno3",
+   port:5432
+});
 
-pool.connect().then(client=>{
-    console.log("Db connected successfully")
-    client.release()
-}).catch(err=>{
-    console.error("Db connection failed",err.stack)
-})
+pool.connect()
+    .then(client => {
+        console.log("Db connected successfully");
+        client.release();
+    })
+    .catch(err => {
+        console.error("Db connection failed", err);
+    });
+
 module.exports = pool;
